d4d9367d377511e31b6fca4da7bb5c3526f27391
yjxy <yjxyml@qq.com>
2022-05-31 14:15:07 +0800

    master更新操作

--- src/main/java/com/packeg/Test02.java
+++ src/main/java/com/packeg/Test02.java
@@ -3,5 +3,6 @@ package com.packeg;
 public class Test02 {
     public static void main(String[] args) {
         System.out.println("test02....");
+        System.out.println("c1客户端操作->test02....");
     }
 }
