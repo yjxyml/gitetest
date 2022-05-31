6f808a392b168cc11aceeb55785b8132beff97bb
yjxy <yjxyml@qq.com>
2022-05-31 13:40:46 +0800

    修改test01.java

--- src/main/java/com/packeg/Test01.java
+++ src/main/java/com/packeg/Test01.java
@@ -3,5 +3,7 @@ package com.packeg;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("test01..........");
+        System.out.println("test02..........");
+        System.out.println("test03..........");
     }
 }
